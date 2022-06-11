fa3b34e9bc02e65a27fed1f009a6e32e50906fab
JasonXuDeveloper - 傑 <jasonxudeveloper@gmail.com>
2022-06-11 19:12:36 +1000

    [feat] new compression stream

--- Nino/Assets/Nino/Shared/CompressMgr.cs
+++ Nino/Assets/Nino/Shared/CompressMgr.cs
@@ -13,7 +13,7 @@ namespace Nino.Shared
         public static byte[] Compress(byte[] data)
         {
             using (var compressedStream = new MemoryStream())
-            using (var zipStream = new GZipStream(compressedStream, CompressionMode.Compress))
+            using (var zipStream = new DeflateStream(compressedStream, CompressionMode.Compress))
             {
                 zipStream.Write(data, 0, data.Length);
                 zipStream.Close();
@@ -29,7 +29,7 @@ namespace Nino.Shared
         public static byte[] Decompress(byte[] data)
         {
             using (var compressedStream = new MemoryStream(data))
-            using (var zipStream = new GZipStream(compressedStream, CompressionMode.Decompress))
+            using (var zipStream = new DeflateStream(compressedStream, CompressionMode.Decompress))
             using (var resultStream = new MemoryStream())
             {
                 zipStream.CopyTo(resultStream);
