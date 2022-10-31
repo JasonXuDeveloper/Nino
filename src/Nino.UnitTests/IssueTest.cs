dd4b293f301e05cde85555364ca9331ee2345e12
JasonXuDeveloper - 傑 <jasonxudeveloper@gmail.com>
2022-10-31 15:39:31 +1100

    [test] issue #33

--- src/Nino.UnitTests/IssueTest.cs
+++ src/Nino.UnitTests/IssueTest.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Nino.Serialization;
 
@@ -6,6 +8,84 @@ namespace Nino.UnitTests
     [TestClass]
     public class IssueTest
     {
+        [TestClass]
+        public class Issue33
+        {
+            [NinoSerialize]
+            [CodeGenIgnore]
+            public partial class GamePatcher
+            {
+
+                [NinoMember(1)] public DateTime StaticValidityDateTime = DateTime.MinValue;
+                [NinoMember(2)] public int CCC= -1;
+                [NinoMember(3)] public string Key = "";
+            }
+            
+            [NinoSerialize]
+            public partial class GamePatcher2
+            {
+
+                [NinoMember(1)] public DateTime StaticValidityDateTime = DateTime.MinValue;
+                [NinoMember(2)] public int CCC= -1;
+                [NinoMember(3)] public string Key = "";
+            }
+            
+            //GENERATED CODE
+            public partial class GamePatcher2
+            {
+                public static GamePatcher2.SerializationHelper NinoSerializationHelper = new GamePatcher2.SerializationHelper();
+                public class SerializationHelper: Nino.Serialization.NinoWrapperBase<GamePatcher2>
+                {
+                    #region NINO_CODEGEN
+                    public override void Serialize(GamePatcher2 value, Nino.Serialization.Writer writer)
+                    {
+                        writer.Write(value.StaticValidityDateTime);
+                        writer.CompressAndWrite(ref value.CCC);
+                        writer.Write(value.Key);
+                    }
+
+                    public override GamePatcher2 Deserialize(Nino.Serialization.Reader reader)
+                    {
+                        GamePatcher2 value = new GamePatcher2();
+                        value.StaticValidityDateTime = reader.ReadDateTime();
+                        reader.DecompressAndReadNumber<System.Int32>(ref value.CCC);
+                        value.Key = reader.ReadString();
+                        return value;
+                    }
+                    #endregion
+                }
+            }
+
+            [TestMethod]
+            public void RunTest()
+            {
+                //no code gen
+                GamePatcher gp = new GamePatcher();
+                gp.CCC=10;
+                gp.Key="ajoaiewrnvo";
+
+                var a = Nino.Serialization.Serializer.Serialize<GamePatcher>(gp);
+                var b = Nino.Serialization.Deserializer.Deserialize<GamePatcher>(a);
+                Assert.AreEqual(gp.CCC, b.CCC);
+                Assert.AreEqual(gp.Key, b.Key);
+                
+                //code gen
+                GamePatcher2 gp2 = new GamePatcher2();
+                gp2.CCC=10;
+                gp2.Key="ajoaiewrnvo";
+
+                var aa = Nino.Serialization.Serializer.Serialize<GamePatcher2>(gp2);
+                
+                Assert.IsTrue(aa.SequenceEqual(a));
+                
+                var bb = Nino.Serialization.Deserializer.Deserialize<GamePatcher>(aa);
+                Assert.AreEqual(gp2.CCC, bb.CCC);
+                Assert.AreEqual(gp2.Key, bb.Key);
+                Assert.AreEqual(gp2.CCC, b.CCC);
+                Assert.AreEqual(gp2.Key, b.Key);
+            }
+        }
+        
         [TestClass]
         public class Issue32
         {
