fa3f1f75346b4b8a5d7b4572cc29a59710ab6259
JasonXuDeveloper - 傑 <jasonxudeveloper@gmail.com>
2023-07-12 16:26:11 +1000

    [update] include all by default

--- src/Nino.Serialization/Attributes/NinoSerializeAttribute.cs
+++ src/Nino.Serialization/Attributes/NinoSerializeAttribute.cs
@@ -12,7 +12,7 @@ namespace Nino.Serialization
 		/// 一个结构体或一个类型的会被序列化以及反序列化
 		/// <param name="includeAll">include all fields and properties</param>
 		/// </summary>
-		public NinoSerializeAttribute(bool includeAll = false)
+		public NinoSerializeAttribute(bool includeAll = true)
 		{
 			IncludeAll = includeAll;
 		}
